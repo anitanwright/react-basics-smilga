02011160c3443b959c85381ca801b8220f1d13f6
Anita Wright <anitanwright@gmail.com>
2022-08-11 09:41:19 -0700

    remove pre-installed template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
 
+function Greeting() {
+    return <h4>This is Anita and this is my first component</h4>;
+    
+}
+
+// function passing in two things- what is being rendered and where it is being rendered
+
+ReactDom.render(<Greeting />, document.getElementById('root'));
