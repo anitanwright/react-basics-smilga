fcb49b1496ca3a2e0beb9edcb5b1612428658153
Anita Wright <anitanwright@gmail.com>
2022-08-31 14:05:37 -0700

    add array to Books object

--- src/index.js
+++ src/index.js
@@ -4,59 +4,62 @@ import ReactDom from 'react-dom';
 
 // css relative path file
 import './index.css';
+const books = [
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL210_SR195,210_.jpg',
+    title: 'Atomic Habits',
+    author: 'James Clear',
+    authorPage: (
+      <a href='https://www.amazon.com/James-Clear/e/B07DJTJC3X/ref=dp_byline_cont_pop_book_1'>
+        Click here to find out more about the author!
+      </a>
+    ),
+  },
 
-// setup vars
-
-const firstBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL210_SR195,210_.jpg',
-  title: 'Atomic Habits',
-  author: 'James Clear',
-  authorPage: (
-    <a href='https://www.amazon.com/James-Clear/e/B07DJTJC3X/ref=dp_byline_cont_pop_book_1'>
-      Click here to find out more about the author!
-    </a>
-  ),
-};
-
-const secondBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/51hb6+asUeL._SX321_BO1,204,203,200_.jpg',
-  title: 'Unaccustomed Earth',
-  author: 'Jhumpa Lahiri',
-  authorPage: (
-    <a href='https://m.media-amazon.com/images/I/91-p01XE3PL._AC_UL640_FMwebp_QL65_.jpg'>
-      Click here to find out more about the author!
-    </a>
-  ),
-};
-
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51hb6+asUeL._SX321_BO1,204,203,200_.jpg',
+    title: 'Unaccustomed Earth',
+    author: 'Jhumpa Lahiri',
+    authorPage: (
+      <a href='https://m.media-amazon.com/images/I/91-p01XE3PL._AC_UL640_FMwebp_QL65_.jpg'>
+        Click here to find out more about the author!
+      </a>
+    ),
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/41lkouDhm5L._SY354_BO1,204,203,200_.jpg',
+    title: 'The Very Hungry Caterpillar',
+    author: 'Eric Carle',
+    authorPage: (
+      <a href='https://www.amazon.com/Eric-Carle/e/B00DVY7LO0/ref=aufs_dp_fta_dsk'>
+        Click here to find out more about the author!
+      </a>
+    ),
+  },
+];
+// using .map to iterate over the array to access the Book obj
 function BookList() {
   return (
     <section className='booklist'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-        authorPage={firstBook.authorPage}
-      />
+      {books.map((book) => {
+        return (
+          <Book book={book}>
 
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-        authorPage={secondBook.authorPage}
-      />
+          </Book>
+        );
+      })}
     </section>
   );
 }
-
-// using props to access and render the properties listed above
+// using props destructuring to access and render the properties listed above
 const Book = (props) => {
+  const { img, title, author, authorPage } = props.book;
   return (
     <article className='book'>
-      <h1>{props.title.toUpperCase()}</h1>
-      <img src={props.img} />
-      <div>{props.author}</div>
-      <p>{props.authorPage}</p>
+      <h1>{title.toUpperCase()}</h1>
+      <img src={img} />
+      <div>{author}</div>
+      <p>{authorPage}</p>
     </article>
   );
 };
